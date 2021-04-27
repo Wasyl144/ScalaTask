fe896292ec527f522b5ed7f988ca48a20051c948
Michal <michal.frankiewicz@hotmail.com>
2021-04-27 15:36:25 +0200

    Initial commit

---
+++ src/main/scala/Main.scala
@@ -0,0 +1,8 @@
+import scala.concurrent.Future
+import scala.concurrent.ExecutionContext.Implicits.global
+import java.nio.file.Paths
+object Main extends App {
+  val PATH : String = Paths.get("user.dir")
+  val FILENAME : String = ""
+  
+}
